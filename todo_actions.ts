6636a02a5fe4f47ce7fab92162d0e26c7ce897f7
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-02-23 01:08:38 +0600

    Note about module

--- todo_actions.ts
+++ todo_actions.ts
@@ -1,3 +1,5 @@
+// any file that will contain top level import or export will be considered as
+// moudule. Module provide local scope for declared function, variable, class
 type filterFun<T> = (a: T) => boolean;
 
 export function getAll<T>(arr: T[], fun: filterFun<T>): T[] {
