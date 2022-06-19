67d8a7c67cf40d74a4423c24c9b53d2e5233c2af
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-06-19 09:54:27 +0600

    Create new ts-config and create express app

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,8 @@
-console.log("Hello node");
+// import express from 'express';
+const express =  require('express');
+
+const app = express();
+app.listen(3000, () => {
+    console.log("Application is running on port 3000");
+    
+});
