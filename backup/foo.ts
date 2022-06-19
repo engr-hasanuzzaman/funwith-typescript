730e80b568d0e478bf4946cc54f16e8e88fcc0f2
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-06-19 22:39:56 +0600

    Experiment module

--- backup/foo.ts
+++ backup/foo.ts
@@ -1,7 +1,12 @@
-setInterval(() => {
-    console.log('-------hello');
-    console.log(__dirname);
-    console.log(__filename);
+export const name = "sumon";
+console.log('------foo is executed');
+
+// setInterval(() => {
+//     console.log('-------hello');
+//     console.log(__dirname);
+//     console.log(__filename);
+//     console.log(module);
     
-    
-}, 500);
+// }, 0);
+
+// export default module;
--- backup/hello.ts
+++ backup/hello.ts
@@ -1,7 +1,11 @@
- var sum = (a: Number, b: any) => {
-  return a + b;
-}
-const s = sum(23, 5);
-var n = "Sumon";
-console.log(s);
-console.log("Name is ", n);
+import {name} from './foo'
+
+console.log(name);
+
+//  var sum = (a: Number, b: any) => {
+//   return a + b;
+// }
+// const s = sum(23, 5);
+// var n = "Sumon";
+// console.log(s);
+// console.log("Name is ", n);
