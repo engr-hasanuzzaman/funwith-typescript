af4d914b05dce2342c63da7272fc71d77a1ff019
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-06-20 22:53:25 +0600

    Experiment path module

--- backup/path.ts
+++ backup/path.ts
@@ -1,2 +1,8 @@
 import path from 'path';
 console.log(path.sep);
+
+const filePath = path.join('test', 'sub-test', 'foo.text');
+console.log(`print the file path ${filePath}`);
+
+const absPath = path.resolve(__dirname, filePath);
+console.log(`absolute path is ${absPath}`);
